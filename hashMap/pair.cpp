7992dc253c956a85ea03cda0ee38c339fb6c0f06
“[ritu <ritu748841@gmail.com>
2025-10-09 12:25:21 +0530

    change

--- hashMap/pair.cpp
+++ hashMap/pair.cpp
@@ -12,11 +12,11 @@ int main()
   // p.second = 22;
   // cout << p.first << " " << p.second << endl;
 
-  // pair<string,pair<int,int>>p;
-  // p.first="kumar";
-  // p.second.first=24;
-  // p.second.second=50;
-  // cout<<p.first<<' '<<p.second.first<<" "<<p.second.second<<endl;
+  pair<string,pair<int,int>>p;
+  p.first="kumar";
+  p.second.first=24;
+  p.second.second=50;
+  cout<<p.first<<' '<<p.second.first<<" "<<p.second.second<<endl;
 
 
 
