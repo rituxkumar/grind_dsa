9dddb459430156f884cb644cdfed20d4c4553abb
Ritu kumar <ritu748841@gmail.com>
2025-10-27 22:05:14 +0530

    done

---
+++ stack/05_triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int a = nums[0], b = nums[1], c = nums[2];
+
+        // Check validity of triangle
+        if (a + b <= c || a + c <= b || b + c <= a)
+            return "not a triangle";
+
+        // Check triangle types
+        if (a == b && b == c)
+            return "equilateral";
+        else if (a == b || b == c || a == c)
+            return "isosceles";
+        else
+            return "scalene";
+    }
+};
